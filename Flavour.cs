9f0239039988ef532eee40e1f506387638867def
KurvyHerv <hervsie@gmail.com>
2024-01-09 14:22:17 +0800

    Filled up Flavour class

--- Flavour.cs
+++ Flavour.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace S10262115_PRG2Assignment
 {
-    internal class Flavour
+    class Flavour
     {
+        public string Type { get; set; }
+        public bool Premium { get; set; }
+        public int Quantity { get; set; }
+        public Flavour() { }
+        public Flavour(string type, bool premium, int quantity)
+        {
+            Type = type;
+            Premium = premium;
+            Quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return ToString();
+        }
     }
 }
