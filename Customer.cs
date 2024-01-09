e0a85d09d0861d0402c3d876fbca934bea19c5f2
xenoroxy <renasoong@gmail.com>
2024-01-10 00:09:54 +0800

    Modify Customer.cs IsBirthday()

--- Customer.cs
+++ Customer.cs
@@ -12,7 +12,7 @@ namespace S10262115_PRG2Assignment
         public int MemberId { get; set; }
         public DateTime Dob { get; set; }
         public Order CurrentOrder { get; set; }
-        public List<Order> OrderHistory { get; set; } = new List<Order>();
+        public List<Order> OrderHistory { get; set; }
         public PointCard Rewards { get; set; }
         public Customer() { }
         public Customer(string name, int memberId, DateTime dob)
@@ -23,11 +23,14 @@ namespace S10262115_PRG2Assignment
         }
         public Order MakeOrder()
         {
-            
+            Console.WriteLine("Enter the order id: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            DateTime timereceived = DateTime.Now;
+            return new Order(id, timereceived);
         }
         public bool IsBirthday()
         {
-
+            return DateTime.Today.Month == Dob.Month && DateTime.Today.Day == Dob.Day;
         }
         public override string ToString()
         {
