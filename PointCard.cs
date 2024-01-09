5de8ef2358c57a343bac8a9eb8e19292284b74b4
KurvyHerv <hervsie@gmail.com>
2024-01-09 14:46:13 +0800

    Created and filled up PointCard Class

---
+++ PointCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10262115_PRG2Assignment
+{
+    class PointCard
+    {
+        public int Points { get; set; }
+        public int PunchCard { get; set; }
+        public string Tier { get; set; }
+        public PointCard() { }
+        public PointCard(int points, int punchCard)
+        {
+            Points = points;
+            PunchCard = punchCard;
+        }
+        public void AddPoints(int points)
+        {
+
+        }
+        public void RedeemPoints(int points)
+        {
+
+        }
+        public void Punch() { }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
