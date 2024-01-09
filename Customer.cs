4ae7d096e4c7fb3ae5d8ed9d2ba399b11481fc3f
KurvyHerv <hervsie@gmail.com>
2024-01-09 14:43:38 +0800

    Created and filled up order class Created and filled up customer class

---
+++ Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10262115_PRG2Assignment
+{
+    class Customer : Order
+    {
+        public string Name { get; set; }
+        public int MemberId { get; set; }
+        public DateTime Dob { get; set; }
+        public Order CurrentOrder { get; set; }
+        public List<Order> OrderHistory { get; set; } = new List<Order>();
+        public PointCard Rewards { get; set; }
+        public Customer() { }
+        public Customer(string name, int memberId, DateTime dob)
+        {
+            Name = name;
+            MemberId = memberId;
+            Dob = dob;
+        }
+        public Order MakeOrder()
+        {
+
+        }
+        public bool IsBirthday()
+        {
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10262115_PRG2Assignment
+{
+    class Order
+    {
+        public int Id {  get; set; }
+        public DateTime TimeReceived { get; set; }
+        public DateTime? TimeFufilled { get; set; }
+        public List<IceCream> IceCreamList { get; set; } = new List<IceCream>();
+        public Order() { }
+        public Order(int id, DateTime timeReceived)
+        {
+            Id = id;
+            TimeReceived = timeReceived;
+        }
+        public void ModifyIceCream(int id)
+        {
+
+        }
+        public void AddIceCream(IceCream iceCream)
+        {
+
+        }
+        public void DeleteIceCream(int id)
+        {
+
+        }
+        public double CalculateTotal()
+        {
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
