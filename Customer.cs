07b4535698f9ccb8504e95ed55c7df4c495543a0
xenoroxy <renasoong@gmail.com>
2024-01-10 00:11:35 +0800

    Modify customer.cs

--- Customer.cs
+++ Customer.cs
@@ -12,7 +12,7 @@ namespace S10262115_PRG2Assignment
         public int MemberId { get; set; }
         public DateTime Dob { get; set; }
         public Order CurrentOrder { get; set; }
-        public List<Order> OrderHistory { get; set; }
+        public List<Order> OrderHistory { get; set; } = new List<Order>();
         public PointCard Rewards { get; set; }
         public Customer() { }
         public Customer(string name, int memberId, DateTime dob)
