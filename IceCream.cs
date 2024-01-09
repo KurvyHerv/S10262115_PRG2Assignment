f825bf58efb8eadb7f370e201b2c34df3545a20f
KurvyHerv <hervsie@gmail.com>
2024-01-09 14:10:08 +0800

    Filled up icecream class

--- IceCream.cs
+++ IceCream.cs
@@ -8,6 +8,26 @@ namespace S10262115_PRG2Assignment
 {
     class IceCream
     {
+        public string Option { get; set; }
+        public int Scoops { get; set; }
+        public List<Flavour> Flavours { get; set; }
+        public List<Topping> Toppings { get; set; }
+        public IceCream() { }
+        public IceCream(string option, int scoops, List<Flavour> flavours, List<Topping> toppings)
+        { 
+            Option = option;
+            Scoops = scoops;
+            Flavours = flavours;
+            Toppings = toppings;
+        }
+        public double CalculatePrice()
+        {
+
+        }
+        public override string ToString()
+        {
+            return ToString();
+        }
 
     }
 }
