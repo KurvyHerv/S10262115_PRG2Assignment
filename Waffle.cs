82b4ba65ebe2fcdd5f4308d0785fc183d49acd7c
KurvyHerv <hervsie@gmail.com>
2024-01-09 14:33:15 +0800

    Filled up waffle class

--- Waffle.cs
+++ Waffle.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace S10262115_PRG2Assignment
 {
-    class Waffle
+    class Waffle : IceCream
     {
+        public string WaffleFlavour { get; set; }
+        public Waffle() { }
+        public Waffle(string options, int scoops, List<Flavour> flavours, List<Topping> toppings, string waffleFlavour) : base(options, scoops, flavours, toppings)
+        {
+            WaffleFlavour = waffleFlavour;
+        }
+        public override double CalculatePrice()
+        {
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
