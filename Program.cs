728d68be1adc673b50a8dce8269cc843265d0cd9
xenoroxy <renasoong@gmail.com>
2024-01-16 12:53:37 +0800

    Add Qn 1

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
 // Student Name : Soong Chu Wen Rena
 // Partner Name : 
 //==========================================================
+
+using S10262115_PRG2Assignment;
+
+//Qn 1
+void DisplayCustomer()
+{
+    string[] lines = File.ReadAllLines("customers.csv");
+    for (int i = 0; i < lines.Length; i++)
+    {
+        string[] line = lines[i].Split(",");
+        Console.WriteLine("{0, -15} {1, -15} {2, -15} {3, -20} {4, -20} {5, -15}",
+            line[0], line[1], line[2], line[3], line[4], line[5]);
+    }
+}
+DisplayCustomer();
