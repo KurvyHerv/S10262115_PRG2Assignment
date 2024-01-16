b667948f3e987a09cf01e2cd6062f6fc32d0292d
KurvyHerv <hervsie@gmail.com>
2024-01-16 12:30:44 +0800

    Added csv data

--- Order.cs
+++ Order.cs
@@ -8,27 +8,65 @@ namespace S10262115_PRG2Assignment
 {
     class Order
     {
-        public int Id {  get; set; }
+        public int Id { get; set; }
         public DateTime TimeReceived { get; set; }
-        public DateTime? TimeFufilled { get; set; }
+        public DateTime? TimeFullfilled { get; set; }
         public List<IceCream> IceCreamList { get; set; } = new List<IceCream>();
         public Order() { }
-        public Order(int id, DateTime timeReceived)
+        public Order(int id, DateTime timereceived)
         {
             Id = id;
-            TimeReceived = timeReceived;
+            TimeReceived = timereceived;
         }
-        public void ModifyIceCream(int id)
+        public void ModifyIceCream(int orderid)
         {
-
+            int orderno = orderid - 1;
+            Console.WriteLine("Enter new option: ");
+            IceCreamList[orderno].Option = Console.ReadLine();
+            Console.WriteLine("Enter new number of scoops: ");
+            IceCreamList[orderno].Scoops = Convert.ToInt32(Console.ReadLine());
+            IceCreamList[orderno].Flavours.Clear();
+            Console.WriteLine("Enter new flavour type (or nil to stop adding): ");
+            string flavourtype = Console.ReadLine();
+            Console.WriteLine("Is it premium? (True/False): ");
+            bool flavourpremium = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Enter new flavour quantity: ");
+            int flavourquantity = Convert.ToInt32(Console.ReadLine());
+            Flavour flavour = new Flavour(flavourtype, flavourpremium, flavourquantity);
+            IceCreamList[orderno].Flavours.Add(flavour);
+            while (flavourtype != "nil")
+            {
+                Console.WriteLine("Enter new flavour (or nil to stop adding): ");
+                flavourtype = Console.ReadLine();
+                Console.WriteLine("Is it premium? (True/False): ");
+                flavourpremium = Convert.ToBoolean(Console.ReadLine());
+                Console.WriteLine("Enter new flavour quantity: ");
+                flavourquantity = Convert.ToInt32(Console.ReadLine());
+                flavour = new Flavour(flavourtype, flavourpremium, flavourquantity);
+                IceCreamList[orderno].Flavours.Add(flavour);
+            }
+            IceCreamList[orderno].Toppings.Clear();
+            Console.WriteLine("Enter new topping (or nil to stop adding): ");
+            string toppingtype = Console.ReadLine();
+            Topping topping = new Topping(toppingtype);
+            IceCreamList[orderno].Toppings.Add(topping);
+            while (toppingtype != "nil")
+            {
+                Console.WriteLine("Enter new topping (or nil to stop adding): ");
+                toppingtype = Console.ReadLine();
+                topping = new Topping(toppingtype);
+                IceCreamList[orderno].Toppings.Add(topping);
+            }
         }
         public void AddIceCream(IceCream iceCream)
         {
             IceCreamList.Add(iceCream);
+            Console.WriteLine("Added successfully.");
         }
-        public void DeleteIceCream(int id)
+        public void DeleteIceCream(int num)
         {
-            IceCreamList.RemoveAt(id - 1);
+            IceCreamList.RemoveAt(num);
+            Console.WriteLine("Deleted successfully.");
         }
         public double CalculateTotal()
         {
@@ -41,7 +79,7 @@ namespace S10262115_PRG2Assignment
         }
         public override string ToString()
         {
-            return Id + "Time received: " + TimeReceived + TimeFufilled + IceCreamList;
+            return "ID: " + Id + "\tTime Received: " + TimeReceived + "\tTime Fullfilled: " + TimeFullfilled + "\tIceCreamList: " + IceCreamList;
         }
     }
 }
